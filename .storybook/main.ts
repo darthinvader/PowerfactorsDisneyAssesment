67b1161286ba9b18013b0e922bc11a701860df10
darthinvader <psaradakisj@gmail.com>
2024-09-14 17:23:34 +0300

    [chore]: updated stories path regex

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,7 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../app/**/*.stories.@(js|jsx|ts|tsx|mdx)', // Updated path to include stories from the 'app' folder
   ],
   addons: [
     '@storybook/addon-onboarding',
